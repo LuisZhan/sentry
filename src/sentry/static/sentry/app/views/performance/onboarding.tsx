693608b533fdba09981501b57448eab7a5b5056e
Dora Chan <dora.lchan@gmail.com>
2020-06-26 15:02:16 -0700

    updated buttons and copy

--- src/sentry/static/sentry/app/views/performance/onboarding.tsx
+++ src/sentry/static/sentry/app/views/performance/onboarding.tsx
@@ -14,23 +14,23 @@ function Onboarding() {
       <h3>{t('Pinpoint problems')}</h3>
       <p>
         {t(
-          "You've got this souped up plan. Now what? Get your software set up. We’ve got endpoints to track down."
+          "You've got this souped up plan. Now what? Get your software set up. We’ve got transactions to track down."
         )}
       </p>
       <ButtonList>
         <Button
           priority="default"
           target="_blank"
-          href="https://docs.sentry.io/performance-monitoring/setup/"
+          href="https://docs.sentry.io/performance-monitoring/performance/"
         >
-          {t('View Features')}
+          {t('Learn More')}
         </Button>
         <Button
           priority="primary"
           target="_blank"
           href="https://docs.sentry.io/performance-monitoring/setup/"
         >
-          {t('Step 1: Install')}
+          {t('Start Setup')}
         </Button>
       </ButtonList>
     </StyledOnboardingPanel>
@@ -40,11 +40,9 @@ function Onboarding() {
 const StyledOnboardingPanel = styled(OnboardingPanel)`
   background: url(${emptyStateImg}) no-repeat 25% center/75%;
 `;
-
 const ButtonList = styled('div')`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(130px, max-content));
   grid-gap: ${space(1)};
 `;
-
 export default Onboarding;
